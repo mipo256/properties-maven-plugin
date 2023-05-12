9b4d78a490715997692352aabbeb591208193614
anikulin <itdetectiv@gmail.com>
2023-05-12 16:35:23 +0300

    Warning log if property already defined

--- src/main/java/io/polivakha/mojo/properties/ReadPropertiesMojo.java
+++ src/main/java/io/polivakha/mojo/properties/ReadPropertiesMojo.java
@@ -23,7 +23,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.List;
+import java.util.Properties;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
